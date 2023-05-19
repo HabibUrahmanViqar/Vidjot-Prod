3bfa4b3b2426eb1d896f90ca670ea5a5958c9574
HabibUrahmanViqar <habibviqar2018@gmail.com>
2023-05-19 23:22:18 +0430

    Third Commit

--- app.js
+++ app.js
@@ -40,7 +40,9 @@ app.set('view engine', 'hbs');
 // body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:500", "https://vidjot-prod.onrender.com"],
+}));
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')))
